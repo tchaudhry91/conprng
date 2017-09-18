b7a9e0002d7de1061bc30edb2da6131dfdd6e183
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2017-09-18 13:08:39 +0530

    Added simple tests

---
+++ prime_generator_test.go
@@ -0,0 +1,57 @@
+package conprng
+
+import (
+	"sort"
+	"testing"
+)
+
+func TestReturnLength(t *testing.T) {
+	testLengths := []int{100, 599, 1000, 2322}
+	for _, length := range testLengths {
+		lenFound := len(GeneratePrimes(length))
+		if length != lenFound {
+			t.Logf("Found %d primes instead of the requested %d", lenFound, length)
+			t.Fail()
+		}
+	}
+}
+
+func TestPrimeAvailability100(t *testing.T) {
+	primes := GeneratePrimes(100)
+	presentNums := []int{2, 3, 17, 457, 331, 281, 541}
+	for _, num := range presentNums {
+		index := sort.SearchInts(primes, num)
+		found := true
+		if index < len(primes) && primes[index] != num {
+			found = false
+		} else if index >= len(primes) {
+			found = false
+		} else {
+			found = true
+		}
+		if found == false {
+			t.Logf("Did not find %d", num)
+			t.Fail()
+		}
+	}
+}
+
+func TestCompositeAbsence100(t *testing.T) {
+	primes := GeneratePrimes(100)
+	presentNums := []int{4, 600, 242, 92, 99, 287, 498}
+	for _, num := range presentNums {
+		index := sort.SearchInts(primes, num)
+		found := true
+		if index < len(primes) && primes[index] != num {
+			found = true
+		} else if index >= len(primes) {
+			found = true
+		} else {
+			found = false
+		}
+		if found == false {
+			t.Logf("Found %d", num)
+			t.Fail()
+		}
+	}
+}
